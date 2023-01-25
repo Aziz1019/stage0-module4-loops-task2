932ca1b763ad8e5659126c9cc125fe091fe123dd
Aziz1019 <starinc1998@gmail.com>
2023-01-25 14:24:28 +0500

    Minor change

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,18 +2,8 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-        String phrase = "";
-        int counter = 0;
-        while (counter < chars.length) {
-            phrase += String.valueOf(chars[counter]);
-            counter++;
+        for (char c : chars){
+            System.out.print(c);
         }
-        System.out.println(phrase);
-    }
-
-    public static void main(String[] args) {
-        WordsBuilder builder = new WordsBuilder();
-        builder.buildPhrase('l', 'o', 'o', 'p', 's', ' ', 'a', 'r', 'e', ' ', 'c', 'o', 'o', 'l');
-
     }
 }
